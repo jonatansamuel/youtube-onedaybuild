8e2456564be9691421b5aa1569aaa96f12da0738
Jonatán Cruz <jonatancruz@Jonatans-iMac.local>
2020-12-30 08:46:12 -0800

    initial xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Jonatán Cruz on 12/30/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
